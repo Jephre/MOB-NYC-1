bb400a0652071b320fbab8c832cebcecc172b78a
Jeffrey Lee <jeffreyhlee86@gmail.com>
2015-01-14 21:48:09 -0500

    no message

---
+++ Lessons/Lesson06/PresentModallyLes6/PresentModallyLes6/GoodWonViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GoodWonViewController.swift
+//  PresentModallyLes6
+//
+//  Created by Jeffrey Lee on 1/14/15.
+//  Copyright (c) 2015 Jeffrey Lee. All rights reserved.
+//
+
+import UIKit
+
+class GoodWonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Lessons/Lesson06/PresentModallyLes6/PresentModallyLes6/PlayerClass.swift
@@ -0,0 +1,9 @@
+//
+//  PlayerClass.swift
+//  PresentModallyLes6
+//
+//  Created by Jeffrey Lee on 1/14/15.
+//  Copyright (c) 2015 Jeffrey Lee. All rights reserved.
+//
+
+import Foundation
