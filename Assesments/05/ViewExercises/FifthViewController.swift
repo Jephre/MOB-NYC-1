f56147079ee43ca53af9774d324b24a5bf60122e
Jeffrey Lee <jeffreyhlee86@gmail.com>
2015-03-02 20:16:34 -0500

    Update HW 5 (4th and 5th View controller)

--- Assesments/05/ViewExercises/FifthViewController.swift
+++ Assesments/05/ViewExercises/FifthViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class FifthViewController: ExerciseViewController {
 
+    var greenButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.exerciseDescription.text = "View 5"
@@ -22,6 +24,32 @@ class FifthViewController: ExerciseViewController {
         
         Your view should be in self.exerciseView, not self.view
         */
+        
+        autoLayout()
+    }
+    
+    func autoLayout() {
+        
+        // setup green button
+        self.exerciseView.addSubview(greenButton)
+        greenButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+        greenButton.backgroundColor = UIColor.greenColor()
+        greenButton.layer.borderColor = UIColor.redColor().CGColor
+        greenButton.layer.borderWidth = 3
+        greenButton.titleLabel?.text = "Tap me!"
+        greenButton.titleLabel?.textColor = UIColor.whiteColor()
+        greenButton.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.exerciseView.addConstraint(NSLayoutConstraint(item: greenButton, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 50))
+        self.exerciseView.addConstraint(NSLayoutConstraint(item: greenButton, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 50))
+        self.exerciseView.addConstraint(NSLayoutConstraint(item: greenButton, attribute: .CenterX, relatedBy: .Equal, toItem: exerciseView, attribute: .CenterX, multiplier: 1.0, constant: 0))
+        self.exerciseView.addConstraint(NSLayoutConstraint(item: greenButton, attribute: .CenterY, relatedBy: .Equal, toItem: exerciseView, attribute: .CenterY, multiplier: 1.0, constant: 0))
+    }
+    
+    // action that occurs when button is tapped
+    func buttonAction(sender:UIButton!)
+    {
+        println("Button tapped")
+        
     }
     
     override func shouldAutorotate() -> Bool {
--- Assesments/05/ViewExercises/FourthViewController.swift
+++ Assesments/05/ViewExercises/FourthViewController.swift
@@ -8,20 +8,21 @@
 
 import UIKit
 
-class FourthViewController: ExerciseViewController {
-
-    var scrollView = UIScrollView()
-    var blueBoxTop = UIView()
-    var purpleLabel = UILabel()
-    var redBoxBottom = UIView()
-    var containerView = UIView()
+class FourthViewController: ExerciseViewController, UIScrollViewDelegate {
+    
+    let scrollView = UIScrollView()
+    let blueBoxTop = UIView()
+    let purpleLabel = UILabel()
+    let redBoxBottom = UIView()
+    let containerView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.exerciseDescription.text = "View 4"
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .Plain, target: self, action: "next")
         /* TODO:
-        Build a scroll view that takes up the entire screen. 
+        Build a scroll view that takes up the entire screen.
         
         In the scroll view, place a blue box at the top (20px high, 10px horizontal margins with the screen, a very tall (1000+px, width the same as the screen) purple label containing white text in the middle, and a red box at the bottom (20px high, 10px horizontal margins with the screen). The scroll view should scroll the entire height of the content.
         
@@ -30,25 +31,26 @@ class FourthViewController: ExerciseViewController {
         
         Your view should be in self.exerciseView, not self.view.
         */
-        var scrollWidth = self.exerciseView.frame.width
-        var scrollHeight = self.toolMinY - self.navMaxY
+        setupScrollView()
+        setupContainerView()
+        blueBoxCreate()
+        //    var scrollWidth = self.exerciseView.frame.width
+        //    var scrollHeight = self.toolMinY - self.navMaxY
         
-        scrollView = UIScrollView(frame: CGRect(x: 0, y: self.navMaxY, width: scrollWidth, height: scrollHeight))
+        //        scrollView = UIScrollView(frame: CGRect(x: 0, y: self.navMaxY, width: scrollWidth, height: scrollHeight))
         
         // scrollView needs view with fixed known dimensions
-        var containerView = UIView(frame: CGRect(x: 0, y: 0, width: scrollWidth, height: 1040))
-        scrollView.addSubview(containerView)
-        scrollView.contentSize = containerView.bounds.size
         
-        self.exerciseView.addSubview(scrollView)
+        //    scrollView.addConstraint(NSLayoutConstraint(item: containerView, attribute: .Width, relatedBy: .Equal, toItem: scrollView, attribute: .Width, multiplier: 1.0, constant: 0))
+        //    scrollView.addConstraint(NSLayoutConstraint(item: containerView, attribute: .Height, relatedBy: .Equal, toItem: scrollView, attribute: .Height, multiplier: 1.0, constant: 0))
         
-        blueBoxCreate()
-        purpleLabelCreate()
-        redBoxCreate()
         
-        containerView.addSubview(blueBoxTop)
-        containerView.addSubview(purpleLabel)
-        containerView.addSubview(redBoxBottom)
+        //        purpleLabelCreate()
+        //        redBoxCreate()
+        
+        
+        //        containerView.addSubview(purpleLabel)
+        //        containerView.addSubview(redBoxBottom)
         
         /* this is the springs and struts way
         scrollView.frame = CGRect(x: 0, y: 0, width: self.exerciseView.frame.width, height: 1000)
@@ -58,7 +60,7 @@ class FourthViewController: ExerciseViewController {
         self.exerciseView.addSubview(scrollView)
         
         var navBarHeight = self.navigationController?.navigationBar.frame.maxY ?? exerciseView.frame.minY
-
+        
         var blueBox = UIView(frame: CGRect(x: 0, y: navBarHeight, width: self.view.bounds.size.width - 20, height: 20))
         blueBox.backgroundColor = UIColor.blueColor()
         blueBox.layoutMargins = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
@@ -69,7 +71,7 @@ class FourthViewController: ExerciseViewController {
         purpleLabel.backgroundColor = UIColor.purpleColor()
         purpleLabel.textColor = UIColor.whiteColor()
         self.scrollView.addSubview(purpleLabel)
-     
+        
         var redBox = UIView(frame: CGRect(x: 0, y: CGRectGetMaxY(purpleLabel.frame), width: self.view.bounds.size.width - 20, height: 20))
         redBox.backgroundColor = UIColor.redColor()
         redBox.layoutMargins = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
@@ -78,41 +80,80 @@ class FourthViewController: ExerciseViewController {
         
     }
     
+    func setupScrollView() {
+        self.exerciseView.addSubview(scrollView)
+        scrollView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.scrollView.delegate = self
+        scrollView.backgroundColor = UIColor.greenColor()
+        
+        let topC = NSLayoutConstraint(item: scrollView, attribute: .Top, relatedBy: .Equal, toItem: exerciseView, attribute: .Top, multiplier: 1.0, constant: 0)
+        let bottomC = NSLayoutConstraint(item: scrollView, attribute: .Bottom, relatedBy: .Equal, toItem: exerciseView, attribute: .Bottom, multiplier: 1.0, constant: 0)
+        
+        let leftC = NSLayoutConstraint(item: scrollView, attribute: .Left, relatedBy: .Equal, toItem: exerciseView, attribute: .Left, multiplier: 1.0, constant: 0)
+        
+        let rightC = NSLayoutConstraint(item: scrollView, attribute: .Right, relatedBy: .Equal, toItem: exerciseView, attribute: .Right, multiplier: 1.0, constant: 0)
+        
+        exerciseView.addConstraints([topC, bottomC, leftC, rightC])
+    }
+    
+    func setupContainerView() {
+        containerView.backgroundColor = UIColor.orangeColor()
+        containerView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        scrollView.addSubview(containerView)
+        scrollView.addConstraint(NSLayoutConstraint(item: containerView, attribute: .Top, relatedBy: .Equal, toItem: scrollView, attribute: .Top, multiplier: 1.0, constant: 0))
+        scrollView.addConstraint(NSLayoutConstraint(item: containerView, attribute: .Left, relatedBy: .Equal, toItem: scrollView, attribute: .Left, multiplier: 1.0, constant: 0))
+        scrollView.addConstraint(NSLayoutConstraint(item: containerView, attribute: .Right, relatedBy: .Equal, toItem: scrollView, attribute: .Right, multiplier: 1.0, constant: 0))
+        scrollView.addConstraint(NSLayoutConstraint(item: containerView, attribute: .Bottom, relatedBy: .Equal, toItem: scrollView, attribute: .Bottom, multiplier: 1.0, constant: 0))
+        scrollView.addConstraint(NSLayoutConstraint(item: containerView, attribute: .Width, relatedBy: NSLayoutRelation.Equal, toItem: scrollView, attribute: NSLayoutAttribute.Width, multiplier: 1.0, constant: 0))
+        containerView.addConstraint(NSLayoutConstraint(item: containerView, attribute: .Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1.0, constant: 2000))
+    }
+    
     func blueBoxCreate() {
+        containerView.addSubview(blueBoxTop)
+        
         blueBoxTop.setTranslatesAutoresizingMaskIntoConstraints(false)
+        // tells program to not using springs/struts paradigm
         blueBoxTop.backgroundColor = UIColor.blueColor()
         
+        //    let topC = NSLayoutConstraint(item: blueBoxTop, attribute: .Top, relatedBy: .Equal, toItem: containerView, attribute: .Top, multiplier: 1.0, constant: 0)
+        //    let bottomC = NSLayoutConstraint(item: blueBoxTop, attribute: .Bottom, relatedBy: .Equal, toItem: containerView, attribute: .Bottom, multiplier: 1.0, constant: 0)
+        //
+        //    let leftC = NSLayoutConstraint(item: blueBoxTop, attribute: .Left, relatedBy: .Equal, toItem: containerView, attribute: .Left, multiplier: 1.0, constant: 0)
+        //
+        //    let rightC = NSLayoutConstraint(item: blueBoxTop, attribute: .Right, relatedBy: .Equal, toItem: containerView, attribute: .Right, multiplier: 1.0, constant: 0)
+        //
+        //    containerView.addConstraints([topC, bottomC, rightC, leftC])
         containerView.addConstraint(NSLayoutConstraint(item: blueBoxTop, attribute: .Top, relatedBy: .Equal, toItem: containerView, attribute: .Top, multiplier: 1.0, constant: 0))
-        containerView.addConstraint(NSLayoutConstraint(item: blueBoxTop, attribute: .LeftMargin, relatedBy: .Equal, toItem: containerView, attribute: .LeftMargin, multiplier: 1.0, constant: 10))
-        containerView.addConstraint(NSLayoutConstraint(item: blueBoxTop, attribute: .RightMargin, relatedBy: .Equal, toItem: containerView, attribute: .RightMargin, multiplier: 1.0, constant: -10))
-        containerView.addConstraint(NSLayoutConstraint(item: blueBoxTop, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 20))
-
+        containerView.addConstraint(NSLayoutConstraint(item: blueBoxTop, attribute: .Left, relatedBy: .Equal, toItem: containerView, attribute: .Left, multiplier: 1.0, constant: 0))
+        containerView.addConstraint(NSLayoutConstraint(item: blueBoxTop, attribute: .Right, relatedBy: .Equal, toItem: containerView, attribute: .Right, multiplier: 1.0, constant: 0))
+        containerView.addConstraint(NSLayoutConstraint(item: blueBoxTop, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1.0, constant: 100))
+        
     }
     
-    func purpleLabelCreate() {
-        purpleLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
-        purpleLabel.backgroundColor = UIColor.purpleColor()
-        purpleLabel.textColor = UIColor.whiteColor()
-        purpleLabel.text = "Hello, World"
-        purpleLabel.textAlignment = NSTextAlignment.Center
-        
-        containerView.addConstraint(NSLayoutConstraint(item: purpleLabel, attribute: .Top, relatedBy: .Equal, toItem: blueBoxTop, attribute: .Bottom, multiplier: 1.0, constant: 0))
-        containerView.addConstraint(NSLayoutConstraint(item: purpleLabel, attribute: .Left, relatedBy: .Equal, toItem: containerView, attribute: .Left, multiplier: 1.0, constant: 0))
-        containerView.addConstraint(NSLayoutConstraint(item: purpleLabel, attribute: .Right, relatedBy: .Equal, toItem: containerView, attribute: .Right, multiplier: 1.0, constant: 0))
-        containerView.addConstraint(NSLayoutConstraint(item: purpleLabel, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 1000))
-
-    }
-    
-    func redBoxCreate() {
-        redBoxBottom.setTranslatesAutoresizingMaskIntoConstraints(false)
-        redBoxBottom.backgroundColor = UIColor.redColor()
-        
-        containerView.addConstraint(NSLayoutConstraint(item: redBoxBottom, attribute: .Top, relatedBy: .Equal, toItem: purpleLabel, attribute: .Bottom, multiplier: 1.0, constant: 0))
-        containerView.addConstraint(NSLayoutConstraint(item: redBoxBottom, attribute: .LeftMargin, relatedBy: .Equal, toItem: containerView, attribute: .LeftMargin, multiplier: 1.0, constant: 10))
-        containerView.addConstraint(NSLayoutConstraint(item: redBoxBottom, attribute: .RightMargin, relatedBy: .Equal, toItem: containerView, attribute: .RightMargin, multiplier: 1.0, constant: -10))
-        containerView.addConstraint(NSLayoutConstraint(item: redBoxBottom, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 20))
-        
-    }
+    //    func purpleLabelCreate() {
+    //        purpleLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+    //        purpleLabel.backgroundColor = UIColor.purpleColor()
+    //        purpleLabel.textColor = UIColor.whiteColor()
+    //        purpleLabel.text = "Hello, World"
+    //        purpleLabel.textAlignment = NSTextAlignment.Center
+    //
+    //        containerView.addConstraint(NSLayoutConstraint(item: purpleLabel, attribute: .Top, relatedBy: .Equal, toItem: blueBoxTop, attribute: .Bottom, multiplier: 1.0, constant: 0))
+    //        containerView.addConstraint(NSLayoutConstraint(item: purpleLabel, attribute: .Left, relatedBy: .Equal, toItem: containerView, attribute: .Left, multiplier: 1.0, constant: 0))
+    //        containerView.addConstraint(NSLayoutConstraint(item: purpleLabel, attribute: .Right, relatedBy: .Equal, toItem: containerView, attribute: .Right, multiplier: 1.0, constant: 0))
+    //        containerView.addConstraint(NSLayoutConstraint(item: purpleLabel, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 1000))
+    //
+    //    }
+    //
+    //    func redBoxCreate() {
+    //        redBoxBottom.setTranslatesAutoresizingMaskIntoConstraints(false)
+    //        redBoxBottom.backgroundColor = UIColor.redColor()
+    //
+    //        containerView.addConstraint(NSLayoutConstraint(item: redBoxBottom, attribute: .Top, relatedBy: .Equal, toItem: purpleLabel, attribute: .Bottom, multiplier: 1.0, constant: 0))
+    //        containerView.addConstraint(NSLayoutConstraint(item: redBoxBottom, attribute: .LeftMargin, relatedBy: .Equal, toItem: containerView, attribute: .LeftMargin, multiplier: 1.0, constant: 10))
+    //        containerView.addConstraint(NSLayoutConstraint(item: redBoxBottom, attribute: .RightMargin, relatedBy: .Equal, toItem: containerView, attribute: .RightMargin, multiplier: 1.0, constant: -10))
+    //        containerView.addConstraint(NSLayoutConstraint(item: redBoxBottom, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 20))
+    //
+    //    }
     
     override func shouldAutorotate() -> Bool {
         return false
@@ -125,5 +166,10 @@ class FourthViewController: ExerciseViewController {
     func next() {
         self.performSegueWithIdentifier("five", sender: nil)
     }
-
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        
+        
+    }
+    
 }
