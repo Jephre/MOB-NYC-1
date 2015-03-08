92e5ba3c8b699db8e71c9e0e6cb0ac3faac273ac
Jeffrey Lee <jeffreyhlee86@gmail.com>
2015-03-08 18:45:17 -0400

    HW 5 finally completed

--- Assesments/05/ViewExercises/FifthViewController.swift
+++ Assesments/05/ViewExercises/FifthViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class FifthViewController: ExerciseViewController {
 
     var greenButton = UIButton()
+    var buttonYAxis: NSLayoutConstraint?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,20 +37,49 @@ class FifthViewController: ExerciseViewController {
         greenButton.backgroundColor = UIColor.greenColor()
         greenButton.layer.borderColor = UIColor.redColor().CGColor
         greenButton.layer.borderWidth = 3
-        greenButton.titleLabel?.text = "Tap me!"
+        greenButton.setTitle("Tap me!", forState: UIControlState.Normal)
         greenButton.titleLabel?.textColor = UIColor.whiteColor()
+        greenButton.titleLabel?.adjustsFontSizeToFitWidth = true
         greenButton.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        
         self.exerciseView.addConstraint(NSLayoutConstraint(item: greenButton, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 50))
         self.exerciseView.addConstraint(NSLayoutConstraint(item: greenButton, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 50))
         self.exerciseView.addConstraint(NSLayoutConstraint(item: greenButton, attribute: .CenterX, relatedBy: .Equal, toItem: exerciseView, attribute: .CenterX, multiplier: 1.0, constant: 0))
-        self.exerciseView.addConstraint(NSLayoutConstraint(item: greenButton, attribute: .CenterY, relatedBy: .Equal, toItem: exerciseView, attribute: .CenterY, multiplier: 1.0, constant: 0))
+        
+        buttonYAxis = NSLayoutConstraint(item: greenButton, attribute: .CenterY, relatedBy: .Equal, toItem: exerciseView, attribute: .CenterY, multiplier: 1.0, constant: 0)
+        
+        if let newYConstraint = buttonYAxis {
+            self.exerciseView.addConstraint(newYConstraint)
+        }
+        
     }
+    
+
     
     // action that occurs when button is tapped
     func buttonAction(sender:UIButton!)
     {
         println("Button tapped")
-        
+        UIView.animateWithDuration(1, animations: {() -> Void in
+            sender.enabled = false
+            sender.backgroundColor = UIColor.redColor()
+            self.buttonYAxis?.constant = -20
+            self.exerciseView.layoutIfNeeded()
+            // adjust y-positioning of the button while the button is disabled
+            
+            }) { (completed) -> Void in
+                UIView.animateWithDuration(0.2, animations: { () -> Void in
+                    sender.backgroundColor = UIColor.greenColor()
+                    self.buttonYAxis?.constant = 0
+                    self.exerciseView.layoutIfNeeded()
+                    // adjust y-position of button back to its original location
+                    
+                    }, completion: { (completed) -> Void in
+                        sender.enabled = true
+                        // when animation cycle of up and down is over, button is back enabled
+                })
+        }
     }
     
     override func shouldAutorotate() -> Bool {
--- Assesments/05/ViewExercises/SixthViewController.swift
+++ Assesments/05/ViewExercises/SixthViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class SixthViewController: ExerciseViewController, UITableViewDelegate, UITableViewDataSource {
     let tableView = UITableView()
     
+//    let items = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.exerciseDescription.text = "View 6"
@@ -32,16 +34,42 @@ class SixthViewController: ExerciseViewController, UITableViewDelegate, UITableV
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        let item = self.items[indexPath.row]
+        
         var cell = tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell?
         if cell == nil {
             cell = UITableViewCell(style: .Default, reuseIdentifier: "Cell")
         }
+        
+//  Springs and Struts way
+//        cell?.textLabel?.backgroundColor = UIColor.greenColor()
+//        cell?.textLabel?.textAlignment = .Center
+//        cell?.textLabel?.text = "Row \(item)"
         
         /* TODO:
         The table view cells on this screen are blank.
         
         Add a label to each cell that is green and centered, and have its text say “Row {X}” (X is the row number of the cell). The tableview should rotate correctly. Use Autolayout.
         */
+        
+        var stringAtTableRow = "Row \(indexPath.row)"
+        
+        var greenLabel = UILabel()
+        
+        greenLabel.text = stringAtTableRow
+        greenLabel.textAlignment = .Center
+        greenLabel.backgroundColor = UIColor.greenColor()
+        greenLabel.layer.cornerRadius = 5
+        greenLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+        greenLabel.layer.borderColor = UIColor.blackColor().CGColor
+        greenLabel.layer.borderWidth = 1
+        
+        cell?.contentView.addSubview(greenLabel)
+        cell?.contentView.addConstraint(NSLayoutConstraint(item: greenLabel, attribute: .CenterX, relatedBy: .Equal, toItem: cell?.contentView, attribute: .CenterX, multiplier: 1.0, constant: 0))
+        cell?.contentView.addConstraint(NSLayoutConstraint(item: greenLabel, attribute: .CenterY, relatedBy: .Equal, toItem: cell?.contentView, attribute: .CenterY, multiplier: 1.0, constant: 0))
+        cell?.contentView.addConstraint(NSLayoutConstraint(item: greenLabel, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 30))
+        cell?.contentView.addConstraint(NSLayoutConstraint(item: greenLabel, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 120))
+        
         
         return cell!
     }
